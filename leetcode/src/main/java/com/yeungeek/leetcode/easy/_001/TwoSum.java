de5729ed2f6652e7a5dbb1a99f4245db09683b8b
yeungeek <yangjian0410@gmail.com>
2019-10-08 23:27:02 +0800

    [ADD] add easy 001

---
+++ leetcode/src/main/java/com/yeungeek/leetcode/easy/_001/TwoSum.java
@@ -0,0 +1,11 @@
+package com.yeungeek.leetcode.easy._001;
+
+public class TwoSum {
+    public static void main(String[] args) {
+
+    }
+
+    public int[] twoSum(int[] nums, int target) {
+        return null;
+    }
+}
